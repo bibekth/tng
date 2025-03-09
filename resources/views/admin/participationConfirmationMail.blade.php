385144ea12eb8f38d293f73c8cdb711434d2b6ce
Bibek Thapa Magar <bibek.thapa0521@gmail.com>
2025-03-09 22:07:35 +0545

    feat: register on events for participation

--- resources/views/admin/participationConfirmationMail.blade.php
+++ resources/views/admin/participationConfirmationMail.blade.php
@@ -1,7 +1,12 @@
 <html>
 <body>
     <h3>Hello, {{ $participant->name }}!</h3>
+    @if($participant->status == 0)
+    <p>Your form has been denied by the Admins.</p>
+    @else
+    <p>Your form has been approved by the Admins.</p>
     <p>Your payment ID is {{ $participant->payment_id }}</p>
+    @endif
     <p>Thank you for participating in our event.</p>
 </body>
 </html>
